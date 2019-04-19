01397beb5a7eadc7881a6d46d9df97fd1fb8265c
zhangxinxin <kamjin1996@163.com>
2019-04-19 10:37:37 +0800

    sss

--- src/main/java/com/zxx/hmy520/graduationdesign/controller/open/api/TestController.java
+++ src/main/java/com/zxx/hmy520/graduationdesign/controller/open/api/TestController.java
@@ -3,6 +3,9 @@ package com.zxx.hmy520.graduationdesign.controller.open.api;
 import com.zxx.hmy520.graduationdesign.base.controller.BaseController;
 import com.zxx.hmy520.graduationdesign.base.controller.result.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "TestController", tags = "【测试接口】")
 public class TestController extends BaseController {
 
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiOperation(value = "测试接口", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiResponse(code = 200, message = "success", response = Result.class)
     public Result<Object> hello() {
         String hello = "你好，测试~";
         return response(hello);
